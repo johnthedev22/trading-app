4d9eaa0f198c93a6674fef47c0b7785099d0f398
John <jgstainsby@hotmail.com>
2025-11-10 09:58:38 +0000

    removed unused icons

--- src/components/button/StockButtons.tsx
+++ src/components/button/StockButtons.tsx
@@ -1,5 +1,5 @@
 //buttons used to manage the stocks as shown in the StockItems component
-import { ChevronLeftIcon, ChevronRightIcon, ArrowLongUpIcon, ArrowLongDownIcon } from "@heroicons/react/24/outline"
+import { ChevronLeftIcon, ChevronRightIcon} from "@heroicons/react/24/outline"
 import { useState, useEffect } from "react"
 import clsx from "clsx"; //conditional class utility
 import { sortByString } from "../../helpers/stockArraySorts";
