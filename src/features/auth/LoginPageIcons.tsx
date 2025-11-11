76c320f2253ac3a8a2124f1b8e1533f2cc16e137
John <jgstainsby@hotmail.com>
2025-11-11 09:27:03 +0000

    added extra icons for the login screen

--- src/features/auth/LoginPageIcons.tsx
+++ src/features/auth/LoginPageIcons.tsx
@@ -1,61 +1,141 @@
-import { ComputerDesktopIcon, CurrencyPoundIcon, CurrencyDollarIcon, CurrencyEuroIcon } from "@heroicons/react/24/outline"
+import { ComputerDesktopIcon, CurrencyPoundIcon, CurrencyDollarIcon, CurrencyEuroIcon, NewspaperIcon, GlobeAmericasIcon } from "@heroicons/react/24/outline"
 import { useTheme } from "../../hooks/useTheme"
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from "react"
 
 export default function LoginPageIcons() {
     const { state } = useTheme()
-    const [actionClassRed, setActionClassRed] = useState("element-initial")
-    const [actionClassGreen, setActionClassGreen] = useState("element-initial")
-    const [actionClassBlue, setActionClassBlue] = useState("element-initial")
+    const elementRef = useRef<SVGSVGElement>(null)
+    const [repeatAnimation, setRepeatAnimation] = useState<boolean>(false)
+    const classNames: string = "w-25 top-1/2 left-1/2 absolute transform -translate-x-1/2 -translate-y-1/2"
 
-    useEffect(()=>{
-        setActionClassRed("element-initial")
-        setActionClassGreen("element-initial")
-        setActionClassBlue("element-initial")
-    },[state.theme])
+    type ActionClassType = {
+        [key:string]: string 
+    }
 
     type ComponentItem = {
         name: string 
         iconStyles: string
+        applyRef?: boolean
         animationEnd?: () => void
     }
+
     //allow any string to access the react element type
     type IconElements = {
         [key: string]: React.ElementType
     }
+
+    const [actionClass, setActionClass] = useState<ActionClassType>({
+        gbp: "element-initial",
+        usd: "element-initial",
+        eur: "element-initial",
+        news: "element-initial",
+        globe: "element-initial"
+    })
+
+    const [actionShake, setActionShake] = useState<ActionClassType>({
+        main: "shake-element",
+        news: "shake-element"
+    })
+
+    useEffect(()=>{
+        setActionClass({
+            gbp: "element-initial",
+            usd: "element-initial",
+            eur: "element-initial",
+            news: "element-initial",
+            globe: "element-initial"
+        })
+
+        setActionShake({
+            main: "shake-element",
+            news: "shake-element"
+        })
+
+        setRepeatAnimation(false)
+    },[state.theme])
+
+    useEffect(() => {
+        const element = elementRef.current
+        
+
+        if(!element || !repeatAnimation) return  
+
+        setActionShake({
+            main: "shake-element",
+            news: "shake-element"
+        })
+        
+        const handleAnimationStart = () => {
+            setActionClass(prev => ({
+            ...prev,
+            news: "element-initial element-final-news",
+            globe: "element-initial element-final-globe",
+            }))
+        }
+
+        element.addEventListener("animationstart", handleAnimationStart)
+
+        //cleanup of the animation
+        return () => element.removeEventListener("animationstart", handleAnimationStart)
+        
+    },[repeatAnimation])
 
     const components:IconElements = {
         ComputerDesktopIcon,
         CurrencyPoundIcon,
         CurrencyDollarIcon,
         CurrencyEuroIcon,
+        NewspaperIcon,
+        GlobeAmericasIcon
+        
     }
 
     const handleAnimationEnd = () => {
-        setActionClassRed(prevValue => prevValue + " element-final-red")
-        setActionClassGreen(prevValue => prevValue + " element-final-green")
-        setActionClassBlue(prevValue => prevValue + " element-final-blue")
-    }
+        setActionClass(prev => ({
+            ...prev,
+            gbp: "element-initial element-final-red",
+            usd: "element-initial element-final-green",
+            eur: "element-initial element-final-blue"
+        }))
+
+        setActionShake({
+            main: "",
+            news: ""
+        })
+
+        setRepeatAnimation(true)
+    };
     
     const iconData:ComponentItem[] = [
         {
-            name: 'ComputerDesktopIcon',
-            iconStyles: state.theme.length > 0 ? "z-10 shake-element text-purple-500 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-100" : "w-100",
+            name: "ComputerDesktopIcon",
+            iconStyles: state.theme.length > 0 ? `${actionShake.main} z-10 text-purple-500 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[330px]` : `${actionShake.main} z-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[330px]`,
+            applyRef: true,
             animationEnd: handleAnimationEnd
         },
         {
-            name: 'CurrencyPoundIcon',
-            iconStyles: state.theme.length > 0 ? `${actionClassRed} shake-element text-orange-500 w-25 top-1/2 left-1/2 absolute transform -translate-x-1/2 -translate-y-1/2` : "w-25"
+            name: "CurrencyPoundIcon",
+            iconStyles: state.theme.length > 0 ? `${actionClass.gbp} shake-element text-red-500 ${classNames}` : `${actionClass.gbp} shake-element ${classNames}`
         },
         {
-            name: 'CurrencyDollarIcon',
-            iconStyles: state.theme.length > 0 ? `${actionClassGreen} shake-element text-green-500 w-25 top-1/2 left-1/2 absolute transform -translate-x-1/2 -translate-y-1/2` : "w-25"
+            name: "CurrencyDollarIcon",
+            iconStyles: state.theme.length > 0 ? `${actionClass.usd} shake-element text-green-500 ${classNames}` : `${actionClass.usd} shake-element ${classNames}`
         },
         {
-            name: 'CurrencyEuroIcon',
-            iconStyles: state.theme.length > 0 ? `${actionClassBlue} shake-element text-blue-500 w-25 top-1/2 left-1/2 absolute transform -translate-x-1/2 -translate-y-1/2` : "w-25"
+            name: "CurrencyEuroIcon",
+            iconStyles: state.theme.length > 0 ? `${actionClass.eur} shake-element text-blue-500 ${classNames}` : `${actionClass.eur} shake-element ${classNames}`
+        },
+        {
+            name: "NewspaperIcon",
+            iconStyles: state.theme.length > 0 ? `${actionClass.news} ${actionShake.news} text-orange-500 ${classNames}` : `${actionClass.news} shake-element ${classNames}`
+        },
+        {
+            name: "GlobeAmericasIcon",
+            iconStyles: state.theme.length > 0 ? `${actionClass.globe} ${actionShake.news} text-yellow-500 ${classNames}` : `${actionClass.globe} shake-element ${classNames}`
         }
     ]
+
+    
     return(
     <>
         {iconData.map((item) => {
@@ -63,10 +143,13 @@ export default function LoginPageIcons() {
 
             if(!Component) return null
 
-            {/* spread operator applies the key: value as a prop to the component */}
-            return <Component key={item.name} className={`${item.iconStyles}`} 
-                        {...(item.animationEnd && { onAnimationEnd: item.animationEnd })}
-                    />
+            const props = {
+                className: item.iconStyles,
+                onAnimationEnd: typeof item.animationEnd === "function" ? item.animationEnd : undefined,
+                ref: item.applyRef ? (elementRef as React.Ref<SVGSVGElement>) : undefined
+            }
+
+            return <Component key={item.name} {...props} />
         })}
         
     </>
