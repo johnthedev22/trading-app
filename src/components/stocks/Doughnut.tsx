888b59e9997200d4ac29ec22fc328eb1964f8a06
John <jgstainsby@hotmail.com>
2025-11-14 11:01:40 +0000

    new idea, give user ability to add further funds during buy so began implmenting that. Adding pulsing effect if buy amount is greater than their wallet encoraging them to deposit more funds

--- src/components/stocks/Doughnut.tsx
+++ src/components/stocks/Doughnut.tsx
@@ -1,3 +1,4 @@
+import { CurrencyPoundIcon } from '@heroicons/react/24/outline';
 import '../../styles/doughnut.css'
 import { useState, useEffect } from 'react';
 
@@ -9,13 +10,16 @@ const Doughnut = ({availableFundsPerc, purchaseFundsPerc}: DoughnutProps) => {
     const [backgroundStyle, setBackgroundStyle] = useState<React.CSSProperties>({
         background: `conic-gradient(green 0% ${availableFundsPerc}%, lightgray 0% ${purchaseFundsPerc}%)`,
     });
+    const [throb, setThrob] = useState<string>("")
 
     useEffect(() => {
         if(purchaseFundsPerc <= 100) {
+            if(throb.length > 0) setThrob("")
             setBackgroundStyle({
                 background: `conic-gradient(green 0% ${availableFundsPerc}%, lightgray 0% ${purchaseFundsPerc}%)`,
             })
         } else {
+            setThrob("animate-element")
             setBackgroundStyle({
                 background: `conic-gradient(lightgray 0% ${100 + availableFundsPerc}%, red 0% ${purchaseFundsPerc}% )`,
             })
@@ -24,15 +28,20 @@ const Doughnut = ({availableFundsPerc, purchaseFundsPerc}: DoughnutProps) => {
     }, [availableFundsPerc, purchaseFundsPerc]);
     
     return(
-    <div className="half-green-donut"
-        style={{
-            width: "47px",
-            height: "47px",
-            borderRadius: "50%",
-            ...backgroundStyle,
-        }}
-        >
-    </div>
+    <button
+        className={`${throb} relative bg-blue-500 hover:bg-blue-700 text-white font-bold 
+            w-8 h-8 rounded-full flex justify-center items-center hover:cursor-pointer`}
+        aria-label="Add item">
+            <div className="absolute half-green-donut"
+            style={{
+                width: "47px",
+                height: "47px",
+                borderRadius: "50%",
+                ...backgroundStyle,
+            }}
+        > </div> 
+        <CurrencyPoundIcon className="h-6 w-6" />
+    </button>
     )
 }
 
